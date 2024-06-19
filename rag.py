58abcdc2a8e3f1dfa9f18f00d819e49c17dc034e
Zhongheng-Cheng <zc2737@columbia.edu>
2024-06-18 21:25:52 -0400

    updated rag with llama-index

---
+++ rag.py
@@ -0,0 +1,16 @@
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.readers.file import XMLReader
+
+data_path = "/mnt/rds/redhen/gallina/projects/ChattyAI/FramesConstructions/fndata-1.7/frame"
+
+parser = XMLReader()
+file_extractor = {".xml": parser}
+documents = SimpleDirectoryReader(
+    data_path, file_extractor=file_extractor
+).load_data()
+
+index = VectorStoreIndex.from_documents(documents)
+
+query_engine = index.as_query_engine()
+response = query_engine.query("What are the FEs of the frame 'Used_up'?")
+print(response)
