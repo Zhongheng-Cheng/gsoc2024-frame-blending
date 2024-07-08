46b1cfd4d953479f7ca4c32f0c067cc9f5b63cde
Zhongheng-Cheng <zc2737@columbia.edu>
2024-07-08 18:53:35 -0400

    updated coding format

--- framenet_hierarchy.py
+++ framenet_hierarchy.py
@@ -1,11 +1,6 @@
 import os
 import json
 
-frame_folder = "frame"
-frames = [file[:-4] for file in os.listdir(frame_folder) if file[-4:] == ".xml"]
-
-x_ing_frames = [frame for frame in frames if frame.split("_")[0].endswith("ing")]
-
 class FrameNode(object):
     def __init__(self, name):
         self.name = name
@@ -64,37 +59,41 @@ class RootFrameNode(FrameNode):
             if node in self.inherited_by.values():
                 self.delete(node.name)
         return
-    
 
-root = RootFrameNode("[root]")
-for frame in frames:
-    is_node_existing = False
-    
-    # parse node name and fathers
-    with open(f"frame_json/{frame}.json", 'r') as fo:
-        data_dict = json.load(fo)
-    frame_name = data_dict.get("frame_name").strip()
-    if frame_name in root.inherited_by.keys():
-        node = root.inherited_by[frame_name]
-        is_node_existing = True
-    else:
-        node = FrameNode(data_dict.get("frame_name").strip())
-    inherits_from = data_dict.get("fr_rel").get("Inherits from")
-    fathers = [father.strip() for father in inherits_from.split(', ')] if inherits_from else []
 
-    # insert node into trees
-    if not fathers and not is_node_existing:
-        root.append_root(node)
-    elif fathers:
-        for father in fathers:
-            father_node = root.find_node(father)
-            if father_node:
-                father_node.inherited_by[node.name] = node
-                if node in root.inherited_by.values():
-                    root.delete(node.name)
-            else:
-                root.append_root(node, [father])
+if __name__ == "__main__":
+    frame_folder = "frame"
+    frames = [file[:-4] for file in os.listdir(frame_folder) if file[-4:] == ".xml"]
+    root = RootFrameNode("[root]")
+    for frame in frames:
+        is_node_existing = False
+        
+        # parse node name and fathers
+        with open(f"frame_json/{frame}.json", 'r') as fo:
+            data_dict = json.load(fo)
+        frame_name = data_dict.get("frame_name").strip()
+        if frame_name in root.inherited_by.keys():
+            node = root.inherited_by[frame_name]
+            is_node_existing = True
+        else:
+            node = FrameNode(data_dict.get("frame_name").strip())
+        inherits_from = data_dict.get("fr_rel").get("Inherits from")
+        fathers = [father.strip() for father in inherits_from.split(', ')] if inherits_from else []
 
-with open("tmp_result.txt", 'w') as fo:
-    fo.write(str(root))
-# print(root.find_node("State"))
+        # insert node into trees
+        if not fathers and not is_node_existing:
+            root.append_root(node)
+        elif fathers:
+            for father in fathers:
+                father_node = root.find_node(father)
+                if father_node:
+                    father_node.inherited_by[node.name] = node
+                    if node in root.inherited_by.values():
+                        root.delete(node.name)
+                else:
+                    root.append_root(node, [father])
+
+    # saving result to file
+    output_filename = "tmp_result.txt"
+    with open(output_filename, 'w') as fo:
+        fo.write(str(root))
