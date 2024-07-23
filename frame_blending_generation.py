8e7181d13128fc7e6d962476c11a8d9de503da5b
Zhongheng-Cheng <zc2737@columbia.edu>
2024-07-23 18:29:41 -0400

    added Window and WindowGroup class; added window switching

--- frame_blending_generation.py
+++ frame_blending_generation.py
@@ -1,116 +1,116 @@
 from frame_hierarchy_analyzer import get_frames, analyze_hierarchy
 import curses
 
-def new_window(title, nlines, ncols, begin_y, begin_x):
-    win = curses.newwin(nlines, ncols, begin_y, begin_x)
-    win.border()
-    win.addstr(0, max(0, (ncols - 1) // 2 - len(title) // 2), title, curses.color_pair(1) | curses.A_BOLD)
-    win.refresh()
-    return win
+class Window():
+    def __init__(self, title, begin_y, begin_x, nlines=None, ncols=None, content=''):
+        self.title = title
+        self.begin_y = begin_y
+        self.begin_x = begin_x
+        lines = content.split('\n')
+        if nlines:
+            self.nlines = nlines + 2
+        else:
+            self.nlines = len(lines) + 2
+        if ncols:
+            self.ncols = ncols + 2
+        else:
+            line_len = max([len(i) for i in lines])
+            title_len = len(title)
+            self.ncols = max(line_len, title_len) + 2
+        self.win = curses.newwin(self.nlines, self.ncols, begin_y, begin_x)
+        self.win.border()
+        self.win.addstr(0, max(0, (self.ncols - len(self.title)) // 2), self.title)
+        self.win.refresh()
+        self.update_content(content)
+        self.focus = False
+        return
+    
+    def end_yx(self):
+        return self.nlines + self.begin_y, self.ncols + self.begin_x
+    
+    def update_focus(self, focus: bool):
+        if focus:
+            self.focus = True
+            self.win.addstr(0, max(0, (self.ncols - len(self.title)) // 2), self.title, curses.color_pair(1) | curses.A_BOLD | curses.A_UNDERLINE)
+        else:
+            self.focus = False
+            self.win.addstr(0, max(0, (self.ncols - len(self.title)) // 2), self.title)
+        self.win.refresh()
+        return
+    
+    def update_content(self, content):
+        lines = content.split('\n')
+        for i, line in enumerate(lines):
+            self.win.addstr(i + 1, 1, line)
+        self.win.refresh()
+
+class WindowGroup():
+    def __init__(self, wins={}):
+        self.focus_index = 0
+        self.wins = wins
+        self.update_windows_focus()
+        return
+    
+    # def __getattribute__(self, __name: str) -> Window:
+    #     return self.wins[__name]
+    
+    def add(self, title, win):
+        self.wins[title] = win
+        return
+    
+    def update_windows_focus(self):
+        for i, win in enumerate(self.wins.values()):
+            if i == self.focus_index:
+                win.update_focus(True)
+            else:
+                win.update_focus(False)
+        return
+    
+    def next_focus(self):
+        self.focus_index = (self.focus_index + 1) % len(self.wins)
+        self.update_windows_focus()
+        return
+    
+    def focus_win(self):
+        title = list(self.wins.keys())[self.focus_index]
+        win = self.wins[title]
+        return win
+    
 
 def main(stdscr):
-    
-    def update_window(name, content, begin_y, begin_x):
-        assert type(content) in [str, list]
-        if type(content) == str:
-            lines = content.split('\n')
-        elif type(content) == list:
-            lines = content
-        nlines = len(lines) + 2
-        ncols = max([len(i) for i in lines]) + 2
-        wins[name] = new_window(title = name,
-                                nlines = nlines,
-                                ncols = ncols,
-                                begin_y = begin_y,
-                                begin_x = begin_x
-                                )
-        for i, line in enumerate(lines):
-            wins[name].addstr(i + 1, 1, line)
-        wins[name].refresh()
-        return nlines + begin_y, ncols + begin_x
 
-    wins = {"main": stdscr}
     curses.curs_set(False)
-    wins["main"].clear()
-    wins["main"].refresh()
-    key_content = [
-        "ctrl + C: Quit",
-        "A: Switch window",
-        "B: 22222",
-        "C: 3333"
-    ]
-    key_end_y, key_end_x = update_window("key", key_content, 0, 0)
-    
-    input_word = ""
-    
+    stdscr.clear()
+    stdscr.refresh()
+
+    win_key_content = """\
+ctrl+C: Quit
+Tab: Switch window
+B: ...
+C: ..."""
+    win_key = Window("key", 0, 0, content=win_key_content)
+
+    win_input_1 = Window("Frame_1", 0, win_key.end_yx()[1], ncols=20)
+    win_input_1.word = ""
+    win_input_2 = Window("Frame_2", 0, win_input_1.end_yx()[1], ncols=20)
+    win_input_2.word = ""
+    wins_group = WindowGroup({win.title: win for win in [win_input_1, win_input_2]})
     
     while True:
-        input1_end_y, input1_end_x = update_window("input_1", f"Enter a word and press Enter: {input_word}", 0, key_end_x)
+
         key = stdscr.getch()
         
         if key == ord('\n'):
-            update_window("frame Relation 1", str(root.find(input_word)), input1_end_y, key_end_x)
-        
+            pass
+        elif key == curses.KEY_TAB: # Tab
+            wins_group.next_focus()
         elif key == curses.KEY_BACKSPACE or key == 127:
-            if len(input_word) > 0:
-                input_word = input_word[:-1]
+            wins_group.focus_win().word = wins_group.focus_win().word[:-1]
         else:
-            input_word += chr(key)
+            wins_group.focus_win().word += chr(key)
+        wins_group.focus_win().update_content(wins_group.focus_win().word)
         
         stdscr.refresh()
-
-
-def tmp_main(stdscr):
-    # Clear screen
-    stdscr.clear()
-    
-    # Initialize windows
-    height, width = 10, 20
-    win1 = curses.newwin(height, width, 0, 0)
-    win2 = curses.newwin(height, width, 0, width)
-    win3 = curses.newwin(height, width, height, 0)
-    win4 = curses.newwin(height, width, height, width)
-    
-    windows = [win1, win2, win3, win4]
-    focus_index = 0
-
-    def draw_borders():
-        for win in windows:
-            win.border(0)
-            win.refresh()
-
-    def update_focus():
-        for i, win in enumerate(windows):
-            if i == focus_index:
-                win.attron(curses.A_REVERSE)
-            else:
-                win.attroff(curses.A_REVERSE)
-            win.refresh()
-
-    draw_borders()
-
-    stdscr.getch()
-    update_focus()
-    stdscr.getch()
-    
-    while True:
-        key = stdscr.getch()
-
-        if key == curses.KEY_UP:
-            focus_index = (focus_index - 2) % 4
-        elif key == curses.KEY_DOWN:
-            focus_index = (focus_index + 2) % 4
-        elif key == curses.KEY_LEFT:
-            focus_index = (focus_index - 1) % 4
-        elif key == curses.KEY_RIGHT:
-            focus_index = (focus_index + 1) % 4
-        elif key == ord('q'):
-            break
-        else:
-            windows[focus_index].addch(1, 1, key)
-            windows[focus_index].refresh()
-
-        update_focus()
 
 
 if __name__ == "__main__":
